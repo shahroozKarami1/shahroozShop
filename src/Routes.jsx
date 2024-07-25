ff47bd09be6480b3f134c656324eb80f59f5f020
shahroozKarami <shahroozdev@gmail.com>
2024-07-25 11:47:42 +0330

    change  config

---
+++ src/Routes.jsx
@@ -0,0 +1,13 @@
+import Home from "./Pages/Home/Home"
+
+
+let  routes   =  [
+    {
+
+path  : "/"    ,    element  :    <Home></Home>
+
+    }
+]
+
+
+export default  routes
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { BrowserRouter } from "react-router-dom";
+
 import './index.css'
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 ReactDOM.createRoot(document.getElementById('root')).render(
+
+
+ <BrowserRouter>
     <App     />
+ </BrowserRouter>
 )
